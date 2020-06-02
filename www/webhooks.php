81c5d6ad148c0f1cfb4d87197d1f57cd99c4a4d3
2ge <444533452@qq.com>
2020-06-02 14:35:11 +0800

    mod test

--- www/webhooks.php
+++ www/webhooks.php
@@ -1,2 +1,11 @@
 <?php
 	echo 'webhooks ok';
+	
+	// git pull
+	
+	// docker exec -it php-fpm-platform composer install
+	
+	// 初始化命令
+	// docker exec -it php-fpm-platform php artisan app:init
+	
+	// docker exec -it php-fpm-platform php artisan app:update
