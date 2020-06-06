87b425f3bb8c40907177c3d9b05cafcc73267770
2ge <444533452@qq.com>
2020-06-06 17:47:04 +0800

    mod 打开git钩子

--- www/webhooks.php
+++ www/webhooks.php
@@ -2,10 +2,18 @@
 	echo 'webhooks ok';
 	
 	// git pull
+	$command = "git pull";
+	shell_exec($command);
 	
 	// docker exec -it php-fpm-platform composer install
+	$command = "docker exec -it php-fpm-platform composer install";
+	shell_exec($command);
 	
 	// 初始化命令
 	// docker exec -it php-fpm-platform php artisan app:init
+	$command = "docker exec -it php-fpm-platform php artisan app:init";
+	shell_exec($command);
 	
 	// docker exec -it php-fpm-platform php artisan app:update
+	$command = "docker exec -it php-fpm-platform php artisan app:update";
+	shell_exec($command);
