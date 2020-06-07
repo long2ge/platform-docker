e449fce6eb8438d01445a041e6fb008443728e78
long2ge <444533452@qq.com>
2020-06-08 01:04:26 +0800

    mod 测试web hook

--- www/webhooks/platform_swoole_webhook.php
+++ www/webhooks/platform_swoole_webhook.php
@@ -35,8 +35,8 @@ $http->on('request', function ($request, $response) {
     $commands = [
         'git pull',
         'cd ../../compose',
-        'docker-compose run --no-deps --rm php-fpm-platform pwd', // 测试默认访问的路径
-        'docker-compose run --no-deps --rm php-fpm-platform composer install',
+        'docker-compose run --rm php-fpm-platform pwd', // 测试默认访问的路径
+        'docker-compose run --rm php-fpm-platform composer install',
         'docker-compose run --no-deps --rm php-fpm-platform php artisan app:init',
         'docker-compose run --no-deps --rm php-fpm-platform php artisan app:update',
         'docker-compose restart php-fpm-platform-swoole', // 启动Swoole
